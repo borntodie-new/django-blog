f98238197585ff8f4ed7068959b96fb7faaa0039
jason <2631937454@qq.com>
2022-07-02 07:58:09 +0800

    配置日志信息&封装日志对象

--- django_blog/local_settings.py
+++ django_blog/local_settings.py
@@ -1,3 +1,5 @@
+import os
+
 from .settings import INSTALLED_APPS, BASE_DIR
 
 # 注册APP
@@ -14,7 +16,57 @@ TIME_ZONE = 'Asia/Shanghai'
 
 USE_TZ = False
 
-# 配置静态文件夹地址
+# 配置静态文件夹路径
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
+
+# 配置日志格式
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(lineno)d %(message)s'
+        },
+        'simple': {
+            'format': '%(levelname)s %(module)s %(lineno)d %(message)s'
+        },
+    },
+    'filters': {
+        'require_debug_true': {
+            '()': 'django.utils.log.RequireDebugTrue',
+        },
+    },
+    'handlers': {
+        'console': {
+            # 实际开发建议使用WARNING
+            'level': 'INFO',
+            'filters': ['require_debug_true'],
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple'
+        },
+        'file': {
+            # 实际开发建议使用ERROR
+            'level': 'DEBUG',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': os.path.join(os.path.dirname(BASE_DIR), "django_blog/logs", "blog.log"),
+            # 日志文件的最大值,这里我们设置300M
+            'maxBytes': 300 * 1024 * 1024,
+            # 日志文件的数量,设置最大日志数量为10
+            'backupCount': 10,
+            # 日志格式:详细格式
+            'formatter': 'verbose',
+            # 文件内容编码
+            'encoding': 'utf-8'
+        },
+    },
+    # 日志对象
+    'loggers': {
+        'django': {
+            # 'handlers': ['console', 'file'],
+            'handlers': ['console', ],
+            'propagate': True,  # 是否让日志信息继续冒泡给其他的日志处理系统
+        },
+    }
+}
