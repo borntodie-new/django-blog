8ec30b1f7246916ec7ef0361508ee56ee60eb4c1
jason <2631937454@qq.com>
2022-07-02 10:34:27 +0800

    简易版的登录逻辑

--- django_blog/urls.py
+++ django_blog/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # 后台路由分发
+    path('backend/', include(('backend.urls', 'backend'), namespace='backend'))
 ]
